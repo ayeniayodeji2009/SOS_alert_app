b7f3ed5d874e29c7df5bb5293c63f2a3cc84ae25
ayeni ayodeji <ayeniayodeji2009@gmail.com>
2026-07-20 23:23:08 +0100

    Adjusting 1st trial of connecting front to backend via code in main.py

--- backend/main.py
+++ backend/main.py
@@ -34,14 +34,16 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Your Vite/React development server
         "http://localhost:3000",   # Alternative React dev server
-        "https://sos-alert-app.vercel.app/",  # Your Render frontend URL (replace with actual)
         "https://sos-alert-app-git-master-ayeniayodeji2009s-projects.vercel.app/",
-        "https://sos-alert-fuk2ippsz-ayeniayodeji2009s-projects.vercel.app/",
+        "https://*.vercel.app",
+        # "https://sos-alert-app.vercel.app/",  # Your Render frontend URL (replace with actual)
         # You can also use ["*"] to allow all origins (not recommended for production)
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allow all headers
+    expose_headers=["*"],
+    max_age=600,  # Cache preflight for 10 minutes
 )
 
 
